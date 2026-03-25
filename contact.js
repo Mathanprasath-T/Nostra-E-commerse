b2e5748dcbe69b1eb61c70b3b36f67085cb8f9c3
Mathanprasath-T <mathanprasath402@gmail.com>
2026-03-25 23:36:36 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,4 +1,22 @@
-var submit=document.querySelector(".submit1");
- submit.addEventListener("click",function(){
-    alert("Thank you! Your message has been sent.");
- })
+import { db } from "./firebase.js";
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+var submit = document.querySelector(".submit1");
+
+submit.addEventListener("click", async function () {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  try {
+    await addDoc(collection(db, "contacts"), {
+      name: name,
+      email: email,
+      message: message
+    });
+
+    alert("✅ Data stored successfully!");
+  } catch (e) {
+    console.log(e);
+  }
+});
